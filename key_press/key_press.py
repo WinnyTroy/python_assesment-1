24c1ea3a6fb52c240174a77f30dd09b28808bd5f
Brian Lusina <lusinabrian@gmail.com>
2016-08-05 21:32:38 +0300

    Create KeyPress docstring

--- key_press/key_press.py
+++ key_press/key_press.py
@@ -5,20 +5,26 @@ class KeyPress(object):
     """
     Create the dictionary to store the numbers as keys and the list of strings as values
     Create a counter variable to store counts whenever a letter is found in the key-value pairing.
-    Convert the input string to uppercase
+    Convert the input string to uppercase for easier dictionary 'parsing'
+    loop through dictionary keys and loop through the strings, checking if each letter in the string is in the value of the key.
     """
     def __init__(self, phrase):
         self.phrase = phrase
 
     def presses(self):
-        # converts the string to upper
+        # converts the string to upper and stores in variable,
         phrase = self.phrase.upper()
-        KEYS = {"1": "1", "2": ["A", "B", "C", "2"], "3": ["D", "E", "F", "3"],
+        # creates a counter variable
+        key_count = 0
+        KEYS = {"1": ["1"], "2": ["A", "B", "C", "2"], "3": ["D", "E", "F", "3"],
                 "4": ["G", "H", "I", "4"], "5": ["J", "K", "L", "5"], "6": ["M", "N", "O", "6"],
                 "7": ["P", "Q", "R", "S", "7"], "8": ["T", "U", "V", "8"], "9": ["W", "X", "Y", "Z", "9"],
                 "*": ["*"], "0": [" ", "0"], "#": ["#"]
                 }
-
+        # perform loop
+        for k in KEYS.keys():
+            for letter in phrase:
+                if letter in KEYS.get(k):
 
 class Tests(unittest.TestCase):
     def test1(self):
@@ -35,5 +41,5 @@ class Tests(unittest.TestCase):
 
     def test4(self):
         press = KeyPress("i 8 2 Many mandazi 4 brekky")
-        self.assertEqual(9, press.presses())
+        self.assertEqual(45, press.presses())
 
