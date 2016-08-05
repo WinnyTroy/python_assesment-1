43abd71fa7655db91fa1452f9eab3ef377e0c48f
Brian Lusina <lusinabrian@gmail.com>
2016-08-05 15:27:24 +0300

    add question three solution two

--- readme_solutions/question_three.py
+++ readme_solutions/question_three.py
@@ -8,8 +8,19 @@ class QuestionThree(object):
     def div_five(self):
         return ",".join([x for x in self.binary_lst.split(",") if int(x, 2) % 5 == 0])
 
+    def div_five_tw0(self):
+        lst = []
+        for x in self.binary_lst.split(","):
+            if int(x, 2) % 5 == 0:
+                lst.append(x)
+        return ",".join(lst)
+
 
 class Tests(unittest.TestCase):
     def test1(self):
         que = QuestionThree("0100,0011,1010,1001")
         self.assertEqual("1010", que.div_five())
+
+    def test2(self):
+        que = QuestionThree("0100,0011,1010,1001")
+        self.assertEqual("1010", que.div_five_tw0())
