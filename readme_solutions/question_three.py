150a52088781f950a90f826b83af643ae290c506
Brian Lusina <lusinabrian@gmail.com>
2016-08-05 13:59:59 +0300

    add Question 3 solutionn

---
+++ readme_solutions/question_three.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+class QuestionThree(object):
+    def __init__(self, binary_lst):
+        self.binary_lst = binary_lst
+
+    def div_five(self):
+        return ",".join([x for x in self.binary_lst.split(",") if int(x) % 5 == 0])
+
+
+class Tests(unittest.TestCase):
+    def test1(self):
+        que = QuestionThree("0100,0011,1010,1001")
+        self.assertEqual("0100,1010", que.div_five())
